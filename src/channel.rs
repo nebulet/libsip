dc766fe9d7eba0b86f526d3507f7eee27fe792f9
Lachlan Sneff <lachlan.sneff@gmail.com>
2018-06-11 23:05:53 -0400

    Fixed channel implementation

--- src/channel.rs
+++ src/channel.rs
@@ -30,12 +30,22 @@ impl WriteChannel {
 pub struct ReadChannel(Handle);
 
 impl ReadChannel {
-    pub fn read(&self, buffer: &mut [u8]) -> (usize, nabi::Result<()>) {
+    pub fn read_raw(&self, buffer: &mut [u8]) -> (usize, nabi::Result<()>) {
         let mut msg_size_out = 0;
         let res: nabi::Result<u32> = unsafe {
             abi::channel_read((self.0).0, buffer.as_mut_ptr(), buffer.len(), &mut msg_size_out)
         }.into();
 
         (msg_size_out, res.map(|_| ()))
+    }
+
+    pub fn read(&self) -> nabi::Result<Vec<u8>> {
+        let mut faux_buf = [0; 0];
+        let (msg_size, _) = self.read_raw(&mut faux_buf);
+
+        let mut buffer = Vec::with_capacity(msg_size);
+        let (_, res) = self.read_raw(&mut buffer);
+
+        res.map(|_| buffer)
     }
 }
